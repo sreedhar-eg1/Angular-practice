bdbf4f995704e3bfff8b06b2f9e58d5ef5d88da7
sreedhar eg <sreedhareg1997@gmail.com>
2024-12-16 12:06:08 +0530

    Feature - lazy loading the component dynamically on condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AdminProfileComponent } from "./basics/admin-profile/admin-profile.component";
 import { UserProfileComponent } from "./basics/user-profile/user-profile.component";
@@ -11,12 +11,28 @@ import { HostComponent } from "./basics/host/host.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-19-feature';
 
-  isAdmin = signal(false)
+  isAdmin = signal(true)
+  profileComponent: {new(): UserProfileComponent | AdminProfileComponent } | null = null
 
   getComponent() {
     return this.isAdmin() ? AdminProfileComponent : UserProfileComponent
+  }
+
+  // With the help of lazy loading
+  async getComponentUsingLazy() {
+    if (this.isAdmin()) {
+      const {AdminProfileComponent} = await import('./basics/admin-profile/admin-profile.component')
+      this.profileComponent = AdminProfileComponent
+    } else {
+      const {UserProfileComponent} = await import('./basics/user-profile/user-profile.component')
+      this.profileComponent = UserProfileComponent
+    }
+  }
+
+  ngOnInit(): void {
+    this.getComponentUsingLazy()
   }
 }
