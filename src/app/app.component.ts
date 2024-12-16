eb229bda45f2a50d21b68b2a58e773c288544efa
sreedhar eg <sreedhareg1997@gmail.com>
2024-12-16 10:08:51 +0530

    Feature - ngComponentOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminProfileComponent } from "./basics/admin-profile/admin-profile.component";
+import { UserProfileComponent } from "./basics/user-profile/user-profile.component";
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdminProfileComponent, UserProfileComponent, NgComponentOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-19-feature';
+
+  isAdmin = signal(false)
+
+  getComponent() {
+    return this.isAdmin() ? AdminProfileComponent : UserProfileComponent
+  }
 }
