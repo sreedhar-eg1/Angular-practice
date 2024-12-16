1d754c547db51f7681e708eeab10697594efb474
sreedhar eg <sreedhareg1997@gmail.com>
2024-12-16 17:01:16 +0530

    feature - switch statement and local template variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AdminProfileComponent } from './basics/admin-profile/admin-profile.component';
 import { UserProfileComponent } from './basics/user-profile/user-profile.component';
-import { NgComponentOutlet } from '@angular/common';
+import { AsyncPipe, NgComponentOutlet } from '@angular/common';
 import { HostComponent } from './basics/host/host.component';
 import { ParentComponent } from './basics/parent/parent.component';
-import { StylesComponent } from "./basics/styles/styles.component";
-import { SizerComponent } from "./basics/sizer/sizer.component";
+import { StylesComponent } from './basics/styles/styles.component';
+import { SizerComponent } from './basics/sizer/sizer.component';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,19 +19,31 @@ import { SizerComponent } from "./basics/sizer/sizer.component";
     HostComponent,
     ParentComponent,
     StylesComponent,
-    SizerComponent
-],
+    SizerComponent,
+    AsyncPipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'angular-19-feature';
-  fontSize = signal(24)
+  fontSize = signal(24);
+  userRole = signal('admin');
 
   isAdmin = signal(true);
   profileComponent: {
     new (): UserProfileComponent | AdminProfileComponent;
   } | null = null;
+
+  users = [
+    {
+      id: 1,
+      name: 'user 1',
+    },
+    { id: 2, name: 'user 2' },
+  ];
+
+  num$ = from(this.users)!;
 
   getComponent() {
     return this.isAdmin() ? AdminProfileComponent : UserProfileComponent;
