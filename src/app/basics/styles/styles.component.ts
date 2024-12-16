cea8f362b4894b856a0b3b47680d4afd1a3f6063
sreedhar eg <sreedhareg1997@gmail.com>
2024-12-16 15:02:15 +0530

    feature - keyevent

--- src/app/basics/styles/styles.component.ts
+++ src/app/basics/styles/styles.component.ts
@@ -10,4 +10,8 @@ export class StylesComponent {
   flexStyle = "display: flex; flex-direction: row"
   inlineStyle1 = "width: 200px; height: 200px; color: red"
   inlineStyle2 = {width: '200px', height: '200px', color: 'yellow'}
+
+  onType(event: Event) {
+    console.log(event)
+  }
 }
