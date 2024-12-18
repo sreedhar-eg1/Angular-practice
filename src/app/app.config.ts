b5f6d1931040d9083245681e0a64cd8afde529fa
sreedhar eg <sreedhareg1997@gmail.com>
2024-12-18 15:34:39 +0530

    feature - Override Root Providers with bootstrapApplication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // Default strategy where # is not included in the url http://localhost:4200/team 
+    // {provide: LocationStrategy, useClass: PathLocationStrategy},
+    // strategy where # is included in the url http://localhost:4200/#/team 
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
 };
