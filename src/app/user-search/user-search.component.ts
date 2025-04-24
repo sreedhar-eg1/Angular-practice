030b93f6516e5fa23ba7aca1b9840ba3964d4a32
sreedhar eg <sreedhareg1997@gmail.com>
2025-04-24 11:20:20 +0530

    completed http resource feature

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -2,6 +2,16 @@ import { Component, effect, resource, signal } from '@angular/core';
 import { API_URL } from './config';
 import { User } from './model';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { httpResource } from '@angular/common/http';
+
+import { z as zod } from 'zod';
+
+const UsersSchema = zod.array(
+  zod.object({
+    id: zod.number(),
+    name: zod.string()
+  })
+)
 
 @Component({
   selector: 'app-user-search',
@@ -38,12 +48,59 @@ export class UserSearchComponent {
   query = signal('');
 
   // with the help of http resource to fetch data
-  users = <User[]>([]);
+  // users = httpResource<User[]>(() => `${API_URL}?name_like=^${this.query()}`, {
+  //   defaultValue: [],
+  //   parse: (rawData) => UsersSchema.parse(rawData)
+  // });
 
+  // configuring the request URL
+  users = httpResource<User[]>(() => ({
+    url:  `${API_URL}`,
+    params: {
+      "name_like": `^${this.query()}`
+    },
+    headers: {
+      'custom_header': 'header_1'
+    }
+  }), {
+    defaultValue: [],
+    parse: (rawData) => UsersSchema.parse(rawData)
+  });
+
+  /* 
+    * with the help of resource and httpResource, the returned value is not a signal, it is httpResource ref
+      hence we cannot read it like a function, for example users()
+      but we need to read it from users.value()
+
+    * whenever a signal specified inside httpResouce changes, Api will be recalled
+
+    * loading state
+      there is a method called isLoading() to get the loading state, actually its a signal, to read signal we call it has a method
+
+    * error handling
+      there is signal called error, we can get the error there
+
+    * recall the API
+      there is signal property called reload to recall the API
+
+    * we can specify the default value, in second parameter with defaultValue key
+
+    * schema validation library
+
+    * parse function in option
+      this function helps us to reshape the respone into another form, or for any validation purpose 
+
+    * we can also configure the request by adding headers
+
+*/
   constructor() {
     // effect(() => {
     //   console.log(this.users.status());
     // })
+
+    effect(() => {
+      this.users.error() && console.log(this.users.error());
+    });
   }
   addUser() {
     const user = { id: 123, name: 'Dmytro Mezhenskyi' };
