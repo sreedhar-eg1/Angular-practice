33bb4f80a30f70c568a571c41a77dbba9a85cd15
sreedhar eg <sreedhareg1997@gmail.com>
2025-04-25 09:53:50 +0530

    completed linked signal

--- src/app/rating-picker/reaction-picker.ts
+++ src/app/rating-picker/reaction-picker.ts
@@ -1,21 +1,38 @@
-import { Component, computed, input, linkedSignal, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  input,
+  linkedSignal,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'reaction-picker',
   template: `
     <ul>
       @for (reaction of reactions(); track $index) {
-        <li [class.selected]="isSelected(reaction)"
-            (click)="selectedReaction.set(reaction)"
-        >{{reaction}}</li>
+      <li
+        [class.selected]="isSelected(reaction)"
+        (click)="selectedReaction.set(reaction)"
+      >
+        {{ reaction }}
+      </li>
       }
     </ul>
-    <span>Selected: <b>{{selectedReaction() || 'Nothing'}}</b></span>
+    <span
+      >Selected: <b>{{ selectedReaction() || 'Nothing' }}</b></span
+    >
   `,
 })
 export class ReactionPicker {
   reactions = input<string[]>([]);
 
+  // linked signal without configuration
+  // selectedReaction = linkedSignal(() => {
+  //   return this.reactions().at(-1)
+  // })
+
+  // linked signal with configuration
   selectedReaction = linkedSignal<string[], string | null>({
     source: () => this.reactions(),
     computation: (source, previous) => {
@@ -23,7 +40,24 @@ export class ReactionPicker {
     }
   });
 
-  protected isSelected(reaction: string){
+  protected isSelected(reaction: string) {
     return this.selectedReaction() === reaction;
   }
+
+  /*
+      * linked signal is a new feature which is under developer preview, not ready for production
+
+      * Its like computed signal, which re run when the signal defined inside it changes
+
+      * but computed signal are readable signal, we cannot write anything. in this case where linkedsignal come into play
+
+      * linked signal are same as computed signal, where we can write anything, and also read. and also we can get previous value
+
+      * linked signal can be configured
+        we can pass object with source and computation key
+        source -> the signal that will change
+        computaion -> can get the source and previous value
+
+      * when we pass generic types to linked signal, we need to pass two type, one for source and other for computation
+  */
 }
